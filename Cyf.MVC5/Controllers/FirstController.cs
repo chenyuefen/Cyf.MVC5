f3b5c6a51bcfade11ec7cf276a68d6d28335d309
越锋 陈 <3510329812@qq.com>
2020-04-24 14:09:25 +0800

    四种传值方式：ViewData、ViewBag、TempData、Model

--- Cyf.MVC5/Controllers/FirstController.cs
+++ Cyf.MVC5/Controllers/FirstController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cyf.MVC5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,7 +26,6 @@ namespace Cyf.MVC5.Controllers
     /// 【广义MVC】(Model--View-Controller)
     ///  V是界面  M是数据和逻辑  C是控制，把M和V链接起来
     ///  程序设计模式，一种设计理念，可以有效的分离界面和业务
-    /// 
     /// 【狭义MVC】是web开发框架
     ///  V--Views   用户看到的视图内容
     ///  C---Controllers  决定用户使用哪个视图，还能调用逻辑计算 
@@ -36,20 +36,85 @@ namespace Cyf.MVC5.Controllers
     /// WebApi是返回数据的，为啥不都用MVC算了
     /// 其实不管是aspx/ashx/webapi/mvc,都是使用Http协议
     /// 所以一切的请求都可以实现的！ 
-    /// Aspx：属于比较重的，默认有页面的生命周期---前后融合，viewstate---跟cs是一一对应
-    /// Ashx: 属于轻量级的，没有页面的概念
-    /// MVC：前后分离的，C可以任意指定视图，可以一套后台多套UI
-    /// WepApi:专人做专事儿，管道都是独立的；RESTful，没有action
+    /// 【Aspx】属于比较重的，默认有页面的生命周期---前后融合，viewstate---跟cs是一一对应
+    /// 【Ashx】属于轻量级的，没有页面的概念
+    /// 【MVC】前后分离的，C可以任意指定视图，可以一套后台多套UI
+    /// 【WepApi】专人做专事儿，管道都是独立的；RESTful，没有action
     ///         .net core二者又融合管道了
+    ///         
+    /// ************  四种传值方式：ViewData、ViewBag、TempData、Model  **************
+    /// 【ViewData】字典传值，里面是object，需要类型转换
+    /// 【ViewBag】 dynamic传值，可以随便属性访问，运行时检测
+    /// 以上二者是会覆盖的，后者为准
+    /// 【model】--适合复杂数据的传递,强类型
+    /// 【TempData】--临时数据，可以跨action后台传递，存在session里面，用一次就清理掉
+    ///         
     /// </summary>
     public class FirstController : Controller
     {
+        private List<CurrentUser> _UserList = new List<CurrentUser>()
+        {
+            new CurrentUser()
+            {
+                Id=1,
+                Name="Z",
+                Account="Administrator",
+                Email="57265177@qq.com",
+                LoginTime=DateTime.Now,
+                Password="123456"
+            },
+            new CurrentUser()
+            {
+                Id=2,
+                Name="白天搬砖",
+                Account="Administrator",
+                Email="57265177@qq.com",
+                LoginTime=DateTime.Now,
+                Password="123456"
+            },
+            new CurrentUser()
+            {
+                Id=3,
+                Name="晚上做梦",
+                Account="Administrator",
+                Email="57265177@qq.com",
+                LoginTime=DateTime.Now,
+                Password="123456"
+            },
+        };
+
+
         // GET: First
         /// <summary>
         /// 在此右键添加视图，会在View下自动生成视图文件
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
+        public ActionResult Index(int id = 3)
+        {
+            //base.HttpContext.Session
+            //System.Web.Mvc.WebViewPage
+            CurrentUser currentUser = this._UserList.FirstOrDefault(u => u.Id == id)
+                ?? this._UserList[0];
+
+            base.ViewData["CurrentUserViewData"] = this._UserList[0];//ViewData
+            base.ViewBag.CurrentUserViewBag = this._UserList[1];//ViewBag
+
+            base.ViewData["TestProp"] = "cx";
+            base.ViewBag.TestProp = "Tenk";
+            base.TempData["TestProp"] = "Spider";//独立存储
+
+            base.TempData["CurrentUserTempData"] = currentUser;
+
+            if (id == 1 || id == 2 || id == 3)
+                return View(this._UserList[2]);
+            else if (id < 10)
+                return View("~/Views/First/Index1.cshtml");
+            else
+                return base.RedirectToAction("TempDataShow");
+        }
+
+        //dynamic
+        public ActionResult TempDataShow()
         {
             return View();
         }
