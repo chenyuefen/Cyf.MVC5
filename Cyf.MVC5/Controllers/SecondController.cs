e4119daf7d0e6e46d42174de654d5ffbd61c1b08
越锋 陈 <3510329812@qq.com>
2020-04-24 17:16:13 +0800

    [Second]路由匹配规则及自定义路由

---
+++ Cyf.MVC5/Controllers/SecondController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cyf.MVC5.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string String()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+        }
+
+        public string Time(int year, int month, int day)
+        {
+            return $"当前传入日期：{year}-{month}-{day}";
+        }
+    }
+}
