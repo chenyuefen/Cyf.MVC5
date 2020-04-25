d0e20c8cf24fdd093296ceeb11f2649236a83e37
越锋 陈 <3510329812@qq.com>
2020-04-25 13:49:23 +0800

    [Second]插件式（Plugins）编程项目创建：拆分区域及控制器

---
+++ Cyf.MVC5.Plugins/Controllers/PluginsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cyf.MVC5.Plugins.Controllers
+{
+    public class PluginsController : Controller
+    {
+        // GET: Plugins
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cyf.MVC5.Plugins/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cyf.MVC5.Plugins.MvcApplication" Language="C#" %>
