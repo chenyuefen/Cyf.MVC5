12b5adcdaa820e19c424405a192ddf2dd3062437
越锋 陈 <3510329812@qq.com>
2020-04-25 13:56:54 +0800

    [Second]Plugins插件式编程：控制器分离

--- Cyf.MVC5.Plugins/Controllers/PluginsController.cs
+++ Cyf.MVC5.Plugins/Controllers/PluginsController.cs
@@ -6,6 +6,14 @@ using System.Web.Mvc;
 
 namespace Cyf.MVC5.Plugins.Controllers
 {
+    /// <summary>
+    /// ************  Plugins插件式编程  **************
+    /// Plugins 控制器：
+    /// ->创建新的MVC5项目
+    /// ->写好控制器以后
+    /// ->在主的项目里边添加引用该项目
+    /// ->然后把Viws下的视图文件都拷贝过去即可
+    /// </summary>
     public class PluginsController : Controller
     {
         // GET: Plugins
