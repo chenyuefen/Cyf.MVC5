cc337dc238ced2968a35f6edb81d58f30da8ce5d
越锋 陈 <3510329812@qq.com>
2020-04-25 14:52:55 +0800

    [Second]插件式开发描述

--- Cyf.MVC5/Controllers/SecondController.cs
+++ Cyf.MVC5/Controllers/SecondController.cs
@@ -20,13 +20,17 @@ namespace Cyf.MVC5.Controllers
     /// 请求进到网站--X--请求地址被路由按顺序匹配--遇到一个吻合的结束---就到对应的控制器和action
     /// 
     /// 
-    /// ************  Area区域：为了拆分项目  **************
+    /// ************  Area区域：为了拆分项目【插件式开发】  **************
     /// 因为一个Web项目可以非常大非常复杂，多人合作开发，命名就成问题了，Area可以把项目拆分开，方便团队合作；演变到后面可以做成插件式开发：
     /// MvcApplication--Application_Start--AreaRegistration.RegisterAllAreas()
     /// 其实就是把SystemAreaRegistration给注册下---添加URL地址规则
     /// 请求来了就匹配(area在普通的之前)
     /// 
+    /// 众所周知，MVC请求的最后是反射调用的controller+action，信息来自于url+route，路由匹配时，只能找到action和controller，  
+    /// 其实还有个步骤，扫描+存储，在bin里面找Controller的子类，然后把命名空间--类名称+全部方法都存起来
     /// 
+    /// a 控制器类可以出现在MVC项目之外，唯一的规则就是继承自Controller
+    /// b Area也可以独立开，规则是必须有个继承AreaRegistration
     /// </summary>
     public class SecondController : Controller
     {
