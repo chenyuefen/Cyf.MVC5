69619abd6c28242a05879d89dfd5dc0c1f367489
越锋 陈 <3510329812@qq.com>
2020-04-25 14:34:27 +0800

    [Second]Plugins插件式编程：区域分离

--- Cyf.MVC5.Plugins/Areas/PluginAreas/Controllers/HomeController.cs
+++ Cyf.MVC5.Plugins/Areas/PluginAreas/Controllers/HomeController.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Cyf.MVC5.Plugins.Controllers
+namespace Cyf.MVC5.Plugins.Areas.PluginAreas.Controllers
 {
     /// <summary>
     /// ************  Plugins插件式编程  **************
-    /// Plugins 控制器：
+    /// Area 区域：
     /// ->创建新的MVC5项目
     /// ->写好控制器以后
     /// ->在主的项目里边添加引用该项目
-    /// ->然后把Views下的视图文件都拷贝过去即可
+    /// ->然后把Views下的视图文件都拷贝过去即可【注意只拷贝Views】
     /// </summary>
-    public class PluginsController : Controller
+    public class HomeController : Controller
     {
-        // GET: Plugins
+        // GET: PluginAreas/Home
         public ActionResult Index()
         {
             return View();
---
+++ Cyf.MVC5.Plugins/Areas/PluginAreas/PluginAreasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Cyf.MVC5.Plugins.Areas.PluginAreas
+{
+    public class PluginAreasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PluginAreas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PluginAreas_default",
+                "PluginAreas/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
